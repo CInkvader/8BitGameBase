6ef24ad9ef756a8535ab9c31ddc503e6a504581a
Ika Musume <invasionikamusume@gmail.com>
2024-03-07 06:53:32 +0800

    Add project files.

---
+++ 8BitGameBase/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _8BitGameBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        
+    }
+}
