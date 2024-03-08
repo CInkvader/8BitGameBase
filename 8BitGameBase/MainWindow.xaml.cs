3c783f109fcdda16e35af4a0f7f40262fc4fd60a
CInkvader <chrisartmlorilla@gmail.com>
2024-03-08 19:55:21 +0800

    Major Update

--- 8BitGameBase/MainWindow.xaml.cs
+++ 8BitGameBase/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using _8BitGameBase.View.UserControls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,20 +12,161 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace _8BitGameBase
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private Random _random = new();
+        private DispatcherTimer _timer = new();
+        private readonly List<BitButton> _buttons = [];
+
+        private int _bitAnswer = 0;
+        private int _roundStartTime = 30;
+
+        private string _tbDecimalQuestion = string.Empty;
+        private string _tbGameTimer = string.Empty;
+        private string _tbGameRound = string.Empty;
+
         public MainWindow()
         {
+
             DataContext = this;
+
+            _timer = new()
+            {
+                Interval = new TimeSpan(0, 0, 0, 1, 0)
+            };
+            _timer.Tick += Timer_Tick;
+
+            _buttons = [];
+            _tbDecimalQuestion = string.Empty;
+            _bitAnswer = 0;
+            TbGameRound = "0";
+
             InitializeComponent();
+            InitializeButtons();
+        }
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            int time = (int.Parse(TbGameTimer) - 1);
+            TbGameTimer = time.ToString();
+
+            if (time == 0)
+            {
+                StopGame();
+            }
+        }
+        private void InitializeButtons()
+        {
+            for (int i = 0, j = 128; i < 8; ++i)
+            {
+                BitButton button = new BitButton();
+                _buttons.Add(button);
+                _buttons[i].BtnBit.Click += BtnBitClicked;
+                _buttons[i].BtnTag = (j /= i == 0 ? 1 : 2);
+
+                ugButtons.Children.Add(button);
+            }
+
+            BtnPlay.BtnPlay.Click += BtnPlay_Click;
+            BtnPlay.BtnContent = "Start Game";
         }
 
-        
+        public string TbDecimalQuestion
+        {
+            get { return _tbDecimalQuestion; }
+            set { _tbDecimalQuestion = value; OnPropertyChanged(); }
+        }   
+        public string TbGameTimer
+        {
+            get { return _tbGameTimer; }
+            set { _tbGameTimer = value; OnPropertyChanged(); }
+        }
+        public string TbGameRound
+        {
+            get { return _tbGameRound; }
+            set { _tbGameRound = value; OnPropertyChanged(); }
+        }
+
+        private void CheckAnswer()
+        {
+            if (_bitAnswer.ToString() == _tbDecimalQuestion)
+            {
+                NewRound();
+            }
+        }
+        private void StartGame()
+        {
+            NewRound();
+            _timer.Start();
+        }
+        private void NewRound()
+        {
+            ResetBitButtons();
+            TbDecimalQuestion = _random.Next(1, 256).ToString();
+
+            TbGameRound = (int.Parse(TbGameRound) + 1).ToString();
+            int round = int.Parse(TbGameRound);
+
+            if (round > 1 && round < 12)
+            {
+                _roundStartTime -= 2;
+            }
+            TbGameTimer = _roundStartTime.ToString();
+        }
+        private void ResetBitButtons()
+        {
+            foreach (BitButton button in _buttons)
+            {
+                button.BtnContent = "0";
+            }
+        }
+        private void StopGame()
+        {
+            ResetBitButtons();
+            TbGameRound = "0";
+            TbGameTimer = "30";
+            TbGameTimer = "0";
+            TbDecimalQuestion = "0";
+
+            StpGame.Visibility = Visibility.Collapsed;
+            GridMenu.Visibility = Visibility.Visible;
+
+            _timer.Stop();
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            GridMenu.Visibility = Visibility.Collapsed;
+            StpGame.Visibility = Visibility.Visible;
+
+            StartGame();
+        }
+        private void BtnBitClicked(object? sender, RoutedEventArgs e)
+        {
+            if (sender == null)
+                return;
+
+            int i = 0; int bitValue = 0;
+            foreach (BitButton button in _buttons)
+            {
+                if (button.BtnBit.Equals((Button)sender))
+                {
+                    button.SetBit();
+                }
+                bitValue += button.BtnBitValue;
+                ++i;
+            }
+            _bitAnswer = bitValue;
+            CheckAnswer();
+        }
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- 8BitGameBase/View/UserControls/BitButton.xaml.cs
+++ 8BitGameBase/View/UserControls/BitButton.xaml.cs
@@ -1,27 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _8BitGameBase.View.UserControls
 {
-    public partial class BitButton : UserControl
+    public partial class BitButton : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
         public BitButton()
         {
             DataContext = this;
-
+            _btnContent = "0";
             InitializeComponent();
+        }
+
+        private string _btnContent = "Play Game";
+        private int _btnTag = 0;
+
+        public string BtnContent
+        {
+            get { return _btnContent; }
+            set { _btnContent = value; OnPropertyChanged(); }
+        }
+        public int BtnTag
+        {
+            get { return _btnTag; }
+            set { _btnTag = value; OnPropertyChanged(); }
+        }
+        public int BtnBitValue
+        {
+            get { return int.Parse(BtnContent) * BtnTag; }
+        }
+
+        public void SetBit()
+        {
+            BtnContent = _btnContent == "0" ? "1" : "0";
+        }
+        private void OnPropertyChanged([CallerMemberName]string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
